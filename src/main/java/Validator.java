cc96c776f2f6d123a6d6a0a8b85321a9fe4924cb
VatBane <70464844+VatBane@users.noreply.github.com>
2022-08-23 14:18:06 +0300

    Validation

--- src/main/java/Validator.java
+++ src/main/java/Validator.java
@@ -1,4 +1,11 @@
+import javax.swing.plaf.basic.BasicMenuUI;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
 public class Validator {
+    private static final Set<Character> symbols = Set.of('+', '-', '*', '/');
+
     public static boolean brackets(String expr) {
         int left = 0, right = 0;
 
@@ -10,5 +17,46 @@ public class Validator {
         }
 
         return left == right;
+    }
+
+    public static boolean operationsNumber(String expr) {
+        int count = 0;
+
+        for(int i = 1; i < expr.length(); i++) {
+            if (symbols.contains(expr.charAt(i))) {
+                count++;
+            } else
+                count = 0;
+            if (count > 2)
+                return false;
+        }
+        return true;
+    }
+
+    public static boolean operationsOrder(String expr) {
+        char[] buffer = new char[2];
+
+        for (int i = 0, j = 0; i < expr.length(); i++) {
+            if (symbols.contains(expr.charAt(i))) {
+                buffer[j] = expr.charAt(i);
+                j++;
+            } else if (j != 0) {
+                buffer = new char[2];
+                j = 0;
+            }
+
+            if (j == 2) {
+                if(!checkOrder(buffer))
+                    return false;
+                j = 0;
+                buffer = new char[2];
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean checkOrder(char[] buffer) {
+        return buffer[1] == '-';
     }
 }
--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,5 +1,33 @@
-import org.junit.*;
+import org.junit.jupiter.api.*;
 
 public class ValidatorTest {
-    Validator validator;
+    @Test
+    public void bracketsTrue() {
+        Assertions.assertTrue(Validator.brackets("()"));
+    }
+
+    @Test
+    public void bracketsFalse() {
+        Assertions.assertFalse(Validator.brackets("(((((())"));
+    }
+
+    @Test
+    public void oneTwoOperations() {
+        Assertions.assertTrue(Validator.operationsNumber("+000*+00"));
+    }
+
+    @Test
+    public void ThreeMoreOperations() {
+        Assertions.assertFalse(Validator.operationsNumber("****////"));
+    }
+
+    @Test
+    public void orderTrue() {
+        Assertions.assertTrue(Validator.operationsOrder("*-"));
+    }
+
+    @Test
+    public void orderFalse() {
+        Assertions.assertFalse(Validator.operationsOrder("-+"));
+    }
 }
