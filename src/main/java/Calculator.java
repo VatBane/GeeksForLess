a5608ae1a0ac043c61ecb63969bb9266e99b7630
VatBane <70464844+VatBane@users.noreply.github.com>
2022-08-23 17:55:57 +0300

    fixed calculations

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,58 +7,62 @@ public class Calculator {
     private Set<Character> operations = Set.of('+', '*', '/', '-');
 
     public float solve(String expr) {
-        List<Float> numbers = new LinkedList<>();
-        List<String> opers = new LinkedList<>();
-        StringBuilder buf = new StringBuilder("");
-        System.out.println(expr);
+        try {
+            List<Float> numbers = new LinkedList<>();
+            List<String> opers = new LinkedList<>();
+            StringBuilder buf = new StringBuilder("");
+            System.out.println(expr);
 
-        int start = 0;
-        int[] brCount = {0, 0};
+            if (!Validator.brackets(expr) || !Validator.operationsNumber(expr) || !Validator.operationsOrder(expr))
+                throw new Exception("Expression is wrong!!!");
 
-        for (int i = 0; i < expr.length(); i++) {
-            if (expr.charAt(i) == '(') {
-                brCount[0]++;
-                if (brCount[0] <= 1)
-                    start = i;
-            } else if (expr.charAt(i) == ')') {
-                brCount[1]++;
+            int start = 0;
+            int[] brCount = {0, 0};
+
+            for (int i = 0; i < expr.length(); i++) {
+                if (expr.charAt(i) == '(') {
+                    brCount[0]++;
+                    if (brCount[0] <= 1)
+                        start = i;
+                } else if (expr.charAt(i) == ')') {
+                    brCount[1]++;
+                }
+
+                if (brCount[0] == brCount[1] && brCount[0] != 0) {
+                    expr = new StringBuilder(expr).replace(start, i + 1, Float.toString(solve(expr.substring(start + 1, i)))).toString();
+                    i = 0;
+                    brCount = new int[]{0, 0};
+                }
             }
 
-            if (brCount[0] == brCount[1] && brCount[0] != 0) {
-                expr = new StringBuilder(expr).replace(start, i + 1, Float.toString(solve(expr.substring(start + 1, i)))).toString();
-                i = 0;
-                brCount = new int[]{0, 0};
-            }
-        }
+            int regime = 0;
 
-        int regime = 0;
-
-        for (int i = 0; i < expr.length(); i++) {
-            if (regime == 0) {
-                if (digits.contains(expr.charAt(i))) {
-                    buf.append(expr.charAt(i));
+            for (int i = 0; i < expr.length(); i++) {
+                if (regime == 0) {
+                    if (digits.contains(expr.charAt(i))) {
+                        buf.append(expr.charAt(i));
+                    } else {
+                        regime = 1;
+                        i--;
+                        if (buf.isEmpty())
+                            continue;
+                        numbers.add(Float.parseFloat(buf.toString()));
+                        buf = new StringBuilder("");
+                    }
                 } else {
-                    regime = 1;
-                    i--;
-                    if (buf.isEmpty())
-                        continue;
+                    if (operations.contains(expr.charAt(i))) {
+                        buf.append(expr.charAt(i));
+                    } else {
+                        regime = 0;
+                        i--;
+                        opers.add(buf.toString());
+                        buf = new StringBuilder("");
+                    }
+                }
+
+                if (i == expr.length() - 1) {
                     numbers.add(Float.parseFloat(buf.toString()));
-                    buf = new StringBuilder("");
                 }
-            } else {
-                if (operations.contains(expr.charAt(i))) {
-                    buf.append(expr.charAt(i));
-                } else {
-                    regime = 0;
-                    i--;
-                    opers.add(buf.toString());
-                    buf = new StringBuilder("");
-                }
-            }
-
-            if (i == expr.length() - 1) {
-                numbers.add(Float.parseFloat(buf.toString()));
-            }
 
 //          if (digits.contains(expr.charAt(i))) {
 //                buf.append(expr.charAt(i));
@@ -69,55 +73,61 @@ public class Calculator {
 //
 //            if (i == expr.length() - 1)
 //                numbers.add(Float.parseFloat(buf.toString()));
-        }
-
-        if (opers.size() == numbers.size()) {
-            numbers.set(0, numbers.get(0) * -1);
-            opers.remove(0);
-        }
-
-        while (true) {
-            if (opers.contains("*-")) {
-                numbers.set(opers.indexOf("*-") + 1, (numbers.get(opers.indexOf("*-")) + 1) * -1);
-                opers.set(opers.indexOf("*-"), "*");
-            } else if (opers.contains("/-")) {
-                numbers.set(opers.indexOf("/-") + 1, (numbers.get(opers.indexOf("/-")) + 1) * -1);
-                opers.set(opers.indexOf("/-"), "/");
-            } else if (opers.contains("+-")) {
-                numbers.set(opers.indexOf("+-") + 1, (numbers.get(opers.indexOf("+-")) + 1) * -1);
-                opers.set(opers.indexOf("+-"), "+");
-            } else if (opers.contains("--")) {
-                numbers.set(opers.indexOf("--") + 1, (numbers.get(opers.indexOf("--")) + 1) * -1);
-                opers.set(opers.indexOf("--"), "-");
-            } else {
-                break;
             }
-        }
 
-        while (opers.size() > 0) {
-            if (opers.contains("*")) {
-                numbers.set(opers.indexOf("*"), (numbers.get(opers.indexOf("*")) * (numbers.get(opers.indexOf("*") + 1))));
-                numbers.remove(opers.indexOf("*") + 1);
-                opers.remove("*");
-            } else if (opers.contains("/")) {
-                numbers.set(opers.indexOf("/"), (numbers.get(opers.indexOf("/")) / (numbers.get(opers.indexOf("/") + 1))));
-                numbers.remove(opers.indexOf("/") + 1);
-                opers.remove("/");
-            } else if (opers.contains("+")) {
-                numbers.set(opers.indexOf("+"), (numbers.get(opers.indexOf("+")) + (numbers.get(opers.indexOf("+") + 1))));
-                numbers.remove(opers.indexOf("+") + 1);
-                opers.remove("+");
-            } else if (opers.contains("-")) {
-                numbers.set(opers.indexOf("-"), (numbers.get(opers.indexOf("-")) - (numbers.get(opers.indexOf("-") + 1))));
-                numbers.remove(opers.indexOf("-") + 1);
-                opers.remove("-");
-            } else {
-                break;
+            if (opers.size() == numbers.size()) {
+                numbers.set(0, numbers.get(0) * -1);
+                opers.remove(0);
             }
+
+            while (true) {
+                if (opers.contains("*-")) {
+                    numbers.set(opers.indexOf("*-") + 1, (numbers.get(opers.indexOf("*-")) + 1) * -1);
+                    opers.set(opers.indexOf("*-"), "*");
+                } else if (opers.contains("/-")) {
+                    numbers.set(opers.indexOf("/-") + 1, (numbers.get(opers.indexOf("/-")) + 1) * -1);
+                    opers.set(opers.indexOf("/-"), "/");
+                } else if (opers.contains("+-")) {
+                    numbers.set(opers.indexOf("+-") + 1, (numbers.get(opers.indexOf("+-")) + 1) * -1);
+                    opers.set(opers.indexOf("+-"), "+");
+                } else if (opers.contains("--")) {
+                    numbers.set(opers.indexOf("--") + 1, (numbers.get(opers.indexOf("--")) + 1) * -1);
+                    opers.set(opers.indexOf("--"), "-");
+                } else {
+                    break;
+                }
+            }
+
+            while (opers.size() > 0) {
+                if (opers.contains("*")) {
+                    numbers.set(opers.indexOf("*"), (numbers.get(opers.indexOf("*")) * (numbers.get(opers.indexOf("*") + 1))));
+                    numbers.remove(opers.indexOf("*") + 1);
+                    opers.remove("*");
+                } else if (opers.contains("/")) {
+                    numbers.set(opers.indexOf("/"), (numbers.get(opers.indexOf("/")) / (numbers.get(opers.indexOf("/") + 1))));
+                    numbers.remove(opers.indexOf("/") + 1);
+                    opers.remove("/");
+                } else {
+                    if (opers.get(0).equals("+")) {
+                        numbers.set(0, numbers.get(0) + numbers.get(1));
+                    } else {
+                        numbers.set(0, numbers.get(0) - numbers.get(1));
+                    }
+                    numbers.remove(1);
+                    opers.remove(0);
+//                    numbers.set(opers.indexOf("+"), (numbers.get(opers.indexOf("+")) + (numbers.get(opers.indexOf("+") + 1))));
+//                    numbers.remove(opers.indexOf("+") + 1);
+//                    opers.remove("+");
+                }
+            }
+
+            System.out.println(numbers.get(0));
+
+            return numbers.get(0);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
-        System.out.println(numbers.toString());
-
-        return numbers.get(0);
+        return 0f;
     }
 }
