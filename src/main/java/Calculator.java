629fbda319c3f1d924acd44786c6c18920707c39
VatBane <70464844+VatBane@users.noreply.github.com>
2022-08-23 17:11:49 +0300

    right calculation of a simple expression

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,13 +1,12 @@
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.Set;
 
 public class Calculator {
     private Set<Character> digits = Set.of('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', ',');
     private Set<Character> operations = Set.of('+', '*', '/', '-');
 
-    public float solve (String expr) {
+    public float solve(String expr) {
         List<Float> numbers = new LinkedList<>();
         List<String> opers = new LinkedList<>();
         StringBuilder buf = new StringBuilder("");
@@ -26,7 +25,8 @@ public class Calculator {
             }
 
             if (brCount[0] == brCount[1] && brCount[0] != 0) {
-                solve(expr.substring(start + 1, i));
+                expr = new StringBuilder(expr).replace(start, i + 1, Float.toString(solve(expr.substring(start + 1, i)))).toString();
+                i = 0;
                 brCount = new int[]{0, 0};
             }
         }
@@ -38,18 +38,20 @@ public class Calculator {
                 if (digits.contains(expr.charAt(i))) {
                     buf.append(expr.charAt(i));
                 } else {
-                    numbers.add(Float.parseFloat(buf.toString()));
                     regime = 1;
                     i--;
+                    if (buf.isEmpty())
+                        continue;
+                    numbers.add(Float.parseFloat(buf.toString()));
                     buf = new StringBuilder("");
                 }
             } else {
                 if (operations.contains(expr.charAt(i))) {
                     buf.append(expr.charAt(i));
                 } else {
-                    opers.add(buf.toString());
                     regime = 0;
                     i--;
+                    opers.add(buf.toString());
                     buf = new StringBuilder("");
                 }
             }
@@ -69,13 +71,53 @@ public class Calculator {
 //                numbers.add(Float.parseFloat(buf.toString()));
         }
 
-//        if (opers.size() == numbers.size()) {
-//
-//        }
+        if (opers.size() == numbers.size()) {
+            numbers.set(0, numbers.get(0) * -1);
+            opers.remove(0);
+        }
 
+        while (true) {
+            if (opers.contains("*-")) {
+                numbers.set(opers.indexOf("*-") + 1, (numbers.get(opers.indexOf("*-")) + 1) * -1);
+                opers.set(opers.indexOf("*-"), "*");
+            } else if (opers.contains("/-")) {
+                numbers.set(opers.indexOf("/-") + 1, (numbers.get(opers.indexOf("/-")) + 1) * -1);
+                opers.set(opers.indexOf("/-"), "/");
+            } else if (opers.contains("+-")) {
+                numbers.set(opers.indexOf("+-") + 1, (numbers.get(opers.indexOf("+-")) + 1) * -1);
+                opers.set(opers.indexOf("+-"), "+");
+            } else if (opers.contains("--")) {
+                numbers.set(opers.indexOf("--") + 1, (numbers.get(opers.indexOf("--")) + 1) * -1);
+                opers.set(opers.indexOf("--"), "-");
+            } else {
+                break;
+            }
+        }
+
+        while (opers.size() > 0) {
+            if (opers.contains("*")) {
+                numbers.set(opers.indexOf("*"), (numbers.get(opers.indexOf("*")) * (numbers.get(opers.indexOf("*") + 1))));
+                numbers.remove(opers.indexOf("*") + 1);
+                opers.remove("*");
+            } else if (opers.contains("/")) {
+                numbers.set(opers.indexOf("/"), (numbers.get(opers.indexOf("/")) / (numbers.get(opers.indexOf("/") + 1))));
+                numbers.remove(opers.indexOf("/") + 1);
+                opers.remove("/");
+            } else if (opers.contains("+")) {
+                numbers.set(opers.indexOf("+"), (numbers.get(opers.indexOf("+")) + (numbers.get(opers.indexOf("+") + 1))));
+                numbers.remove(opers.indexOf("+") + 1);
+                opers.remove("+");
+            } else if (opers.contains("-")) {
+                numbers.set(opers.indexOf("-"), (numbers.get(opers.indexOf("-")) - (numbers.get(opers.indexOf("-") + 1))));
+                numbers.remove(opers.indexOf("-") + 1);
+                opers.remove("-");
+            } else {
+                break;
+            }
+        }
 
         System.out.println(numbers.toString());
 
-        return 0f;
+        return numbers.get(0);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        calculator.solve("(3+4)-(5+8)*(4-(2-7))");
+        calculator.solve("(-3+4)-(5+8)*(4-(2-7))");
     }
 }
